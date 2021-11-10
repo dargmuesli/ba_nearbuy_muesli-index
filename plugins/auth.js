8f1bdc88c8b2f036d18ee05d3ba825f7a1f66de9
Jonas Thelemann <e-mail@jonas-thelemann.de>
2021-11-10 22:58:20 +0100

    fix(auth): correct i18n

--- plugins/auth.js
+++ plugins/auth.js
@@ -1,4 +1,11 @@
 export default function ({ app }) {
+  const redirect = app.$auth.$storage.options.redirect
+  for (const key in redirect) {
+    redirect[key] =
+      (app.i18n.locale === 'en' ? '' : '/' + app.i18n.locale) + redirect[key]
+  }
+  app.$auth.$storage.options.redirect = redirect
+
   app.$auth.onError((error, name, _endpoint) => {
     console.error(name, error)
   })
